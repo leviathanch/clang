a5349ab5530abb836e8cc36edbcc2c338acb7f3b
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2018-02-19 14:52:43 +0800

    Changing to data layout from llvm-spirv

--- lib/Basic/Targets/SPIR.h
+++ lib/Basic/Targets/SPIR.h
@@ -93,6 +93,7 @@ public:
     getSupportedOpenCLOpts().supportAll();
   }
 };
+
 class LLVM_LIBRARY_VISIBILITY SPIR32TargetInfo : public SPIRTargetInfo {
 public:
   SPIR32TargetInfo(const llvm::Triple &Triple, const TargetOptions &Opts)
@@ -100,8 +101,11 @@ public:
     PointerWidth = PointerAlign = 32;
     SizeType = TargetInfo::UnsignedInt;
     PtrDiffType = IntPtrType = TargetInfo::SignedInt;
-    resetDataLayout("e-p:32:32-i64:64-v16:16-v24:32-v32:32-v48:64-"
-                    "v96:128-v192:256-v256:256-v512:512-v1024:1024");
+    resetDataLayout("e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32"\
+                          "-i64:64:64-f32:32:32-f64:64:64-v16:16:16-v24:32:32"\
+                          "-v32:32:32-v48:64:64-v64:64:64-v96:128:128"\
+                          "-v128:128:128-v192:256:256-v256:256:256"\
+                          "-v512:512:512-v1024:1024:1024");
   }
 
   void getTargetDefines(const LangOptions &Opts,
@@ -115,8 +119,11 @@ public:
     PointerWidth = PointerAlign = 64;
     SizeType = TargetInfo::UnsignedLong;
     PtrDiffType = IntPtrType = TargetInfo::SignedLong;
-    resetDataLayout("e-i64:64-v16:16-v24:32-v32:32-v48:64-"
-                    "v96:128-v192:256-v256:256-v512:512-v1024:1024");
+    resetDataLayout("e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32"\
+                          "-i64:64:64-f32:32:32-f64:64:64-v16:16:16-v24:32:32"\
+                          "-v32:32:32-v48:64:64-v64:64:64-v96:128:128"\
+                          "-v128:128:128-v192:256:256-v256:256:256"\
+                          "-v512:512:512-v1024:1024:1024");
   }
 
   void getTargetDefines(const LangOptions &Opts,
